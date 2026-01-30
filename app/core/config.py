3a439cf8514e220f00d8ff26840fe334f395fcf6
Aleksandra Peskova <peskoval88@gmail.com>
2026-01-30 16:38:01 +0300

    readme

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).parent.parent
 class Settings(BaseSettings):
     app_title: str = 'Благотворительный фонд поддержки котиков QRKot'
     app_description: str = 'Сервис для поддержки котиков'
-    database_url: str | None = None
+    database_url: str
     model_config = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8',
